fbca5327b24be35e538a933e9dcdc49b99d00917
kwlg <kwlg@users.noreply.github.com>
2017-10-19 19:29:50 +0200

    Print raw file date

--- show.py
+++ show.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 from flask import Flask
-from flask import render_template
+from flask import Response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     with open("2017-10-19", "r") as f:
         content = f.read()
-    return render_template('log.html', content=content)
+    return Response(content, mimetype='text/plain')
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
