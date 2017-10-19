31c94a70ecd0584e13cf5619cbda31fde3fdcdcf
kwlg <kwlg@users.noreply.github.com>
2017-10-19 19:26:47 +0200

    Print file contents

--- show.py
+++ show.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return app.send_static_file(plot.txt); #should not be static i think....
+    with open("2017-10-19", "r") as f:
+        content = f.read()
+    return render_template('log.html', content=content)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
