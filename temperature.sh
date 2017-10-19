c73efbd45a85ab97478e1a6a10e0ac0fc96d6f2b
kwlg <kwlg@users.noreply.github.com>
2017-10-19 17:35:20 +0200

    Initial commit: add script that gets temperature from file

---
+++ temperature.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DEV_DIR=`ls -td -- /sys/bus/w1/devices/w1_bus_master1/28-*/ | head -n 1`
+if [ ! -d ${DEV_DIR} ]
+then
+    echo "No device folder exists" 2>&1
+    exit 1
+fi
+
+DEV_FILE="${DEV_DIR}w1_slave"
+
+if [ ! -f ${DEV_FILE} ]
+then
+    echo "No device file exists" 2>&1
+    exit 1
+fi
+
+TEMPERATURE=$(cat ${DEV_FILE} | tail -n1 | awk '{print $10}')
+TEMPERATURE="${TEMPERATURE:2:2}.${TEMPERATURE:4:7}"
+TIME=`date "+%H:%M:%S"`
+
+echo "${TIME},${TEMPERATURE},"
