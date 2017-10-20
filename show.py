0f60c3d5d02118fa9263d5c9d2b9bc7ade24cfce
Gadeerian <rigelerik@gmail.com>
2017-10-20 08:51:55 +0000

    amazing stuff

--- show.py
+++ show.py
@@ -11,5 +11,28 @@ def hello():
         content = f.read()
     return Response(content, mimetype='text/plain')
 
+
+def plot():
+    min=-5
+    max=30
+    plotlines = [[str(i).zfill(2)] for i in range(max,min,-1)]
+    print plotlines
+
+    with open(time.strftime("%Y-%m-%d"), "r") as f:
+        lines = f.readlines()
+    for l in lines:
+        value = int(round(float(l.strip().split(',')[1])))
+        linesDown = max-value
+        for i in range(len(plotlines)):
+            if i == (linesDown-1):
+                plotlines[i].append('X')
+            else:
+                plotlines[i].append('.')
+    
+    done = ''
+    for i in range(len(plotlines)):
+        done += (''.join(plotlines[i]) + '\n')
+    return done
+
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
