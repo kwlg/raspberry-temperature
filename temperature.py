3354219351e5416772bfccd1c48a7083ffe51b0f
Gadeerian <rigelerik@gmail.com>
2017-10-20 08:41:40 +0000

    python temperature

---
+++ temperature.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import os
+import datetime
+import Adafruit_DHT
+
+now = datetime.datetime.now()
+
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+humidity2, temperature2 = Adafruit_DHT.read_retry(11, 17)
+
+line = '{},{},{},{},{}'.format(now.strftime("%H:%M:%S"), temperature, humidity, temperature2, humidity2)
+print line
