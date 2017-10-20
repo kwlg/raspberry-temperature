7eb99a7b2b54039686559f3db808f5260c1433aa
kwlg <kwlg@users.noreply.github.com>
2017-10-20 10:47:46 +0200

    Plot temperature in flask

--- show.py
+++ show.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 from flask import Flask
 from flask import Response
+import subprocess
 import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    with open(time.strftime("%Y-%m-%d"), "r") as f:
+    cmd = ["./plot_temp.sh"]
+    with open('temperature_plot.txt', 'w') as fout:
+        subprocess.Popen(cmd, stdout=fout,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE)
+
+    time.sleep(5)
+
+    with open('temperature_plot.txt', 'r') as f:
         content = f.read()
     return Response(content, mimetype='text/plain')
 
