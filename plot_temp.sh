29039ed7a2e3e414b070b8c1aad7cad393a369b9
kwlg <kwlg@users.noreply.github.com>
2017-10-20 11:11:59 +0200

    Print the max and min temperatures

--- plot_temp.sh
+++ plot_temp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Specify max and min temperatures
-MIN_TEMP=5
-MAX_TEMP=30
-RANGE=$((${MAX_TEMP} - ${MIN_TEMP}))
+MIN_RANGE=5
+MAX_RANGE=30
+RANGE=$((${MAX_RANGE} - ${MIN_RANGE}))
 
 # The file name to read from
 FILE_NAME=`date "+%Y-%m-%d"`
@@ -17,16 +17,33 @@ do
     CONTENTS=`echo ${LINE} | cut -d, -f2-`
     CONTENTS=`printf "%.*f\n" 0 ${CONTENTS::-1}`
     TEMPERATURES+=(${CONTENTS})
+
+    if [ ${#TEMPERATURES[@]} -eq 1 ]
+    then
+        MAX_TEMP=${CONTENTS}
+        MIN_TEMP=${CONTENTS}
+    else
+        if [ ${CONTENTS} -lt ${MIN_TEMP} ]
+        then
+            MIN_TEMP=${CONTENTS}
+        fi
+
+        if [ ${CONTENTS} -gt ${MAX_TEMP} ]
+        then
+            MAX_TEMP=${CONTENTS}
+        fi
+    fi
+
 done < ${FILE_NAME}
 
 # Loop over the temperature range and the temperatures
 for I in `seq 0 ${RANGE}`
 do
-    printf "%3d " $((${MAX_TEMP} - ${I}))
+    printf "%3d " $((${MAX_RANGE} - ${I}))
 
     for TEMP in ${TEMPERATURES[*]}
     do
-        if [ $((${MAX_TEMP} - ${I})) -eq ${TEMP} ]
+        if [ $((${MAX_RANGE} - ${I})) -eq ${TEMP} ]
         then
             echo -n "+"
         else
@@ -36,4 +53,6 @@ do
     echo ""
 done
 
-echo -e "\nCurrent: ${TEMPERATURES[-1]} C"
+echo -e "\nCurrent: ${TEMPERATURES[-1]} C\n"
+echo "Min: ${MIN_TEMP} C"
+echo "Max: ${MAX_TEMP} C"
